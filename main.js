1968a154afd12188c1e764daa8a6defe22aac2f6
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2021-12-24 14:39:08 +0700

    update tabs with indicator

--- main.js
+++ main.js
@@ -42,4 +42,18 @@ overLay.addEventListener("click", function () {
     overLay.style.display = "none"
     navMenu.style.transform = "translateX(-100%)"
 })
-
+//
+// CONTENT 4
+const tabHeader = document.getElementsByClassName("tab-header")[0];
+const tabIndicator = document.getElementsByClassName("tab-indicator")[0];
+const tabBody = document.getElementsByClassName("tab-body")[0];
+const tabsPane = tabHeader.getElementsByTagName("div");
+for (let i = 0; i < tabsPane.length; i++) {
+    tabsPane[i].addEventListener("click", function () {
+        tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+        tabsPane[i].classList.add("active");
+        tabBody.getElementsByClassName("active")[0].classList.remove("active");
+        tabBody.getElementsByTagName("div")[i].classList.add("active");
+        tabIndicator.style.left = `calc((100%/3) * ${i})`;
+    });
+}
